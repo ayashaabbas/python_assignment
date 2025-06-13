4056ba6e54756ebca71ece267bfbec27543763df
ayashaabbas <toayashaabbas@gmail.com>
2025-06-13 22:28:09 +0530

    update

--- Assignment_2/Task_2.py
+++ Assignment_2/Task_2.py
@@ -3,8 +3,8 @@
 # for loop to iterate over numbers from 1 to 50.
 #Calculates the sum of all integers in this range.
 #  Displays the final sum
-sum=0
+Total=0
 for i in range (1,51):
-    sum=sum+i
+    total=total+i
 
-print("The sum of numbers from 1 to 50 is:", sum)
+print("The sum of numbers from 1 to 50 is:", total)
