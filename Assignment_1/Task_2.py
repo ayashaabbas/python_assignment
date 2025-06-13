4913d33c13291be790dfed02d9b22c0a0caac113
ayashaabbas <toayashaabbas@gmail.com>
2025-06-13 22:25:53 +0530

    update

--- Assignment_1/Task_2.py
+++ Assignment_1/Task_2.py
@@ -4,12 +4,12 @@ Task 2: Create a Personalized Greeting Problem Statement: Write a Python program
 '''
 #Takes a user's first name and last name as input.
 
-a=input("Enter your First name :")
-b= input("Enter your last name :")
+first=input("Enter your First name :")
+last= input("Enter your last name :")
 
 #Concatenated the first name and last name into a full name.
 
-c= a+" "+b
+c= first+" "+last
 
 #Printed a personalized greeting message using the full name.
 
